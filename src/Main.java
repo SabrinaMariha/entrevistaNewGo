6b29af512106e49888ceccb5ade70ed514475666
Sabrina Mariha <sabrinammgs@gmail.com>
2024-06-12 16:14:09 -0300

    Ajuste de validação utilizando os métodos da classe senha e interação com o usuário.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Senha senha;
+
+        Scanner sc = new Scanner(System.in);
+        String ap = """
+                    ● Tamanho mínimo: 10;
+                    ● Tamanho máximo: 30;
+                    ● Tem que ter ao menos uma letra maiúscula;
+                    ● Tem que ter ao menos uma letra minúscula;
+                    ● Tem que ter ao menos um número;
+                    ● Tem que ter ao menos um símbolo (qualquer símbolo que não é
+                    caractere nem dígito);\n
+                    """;
+        System.out.println(ap);
+        boolean senhaValida=false;
+        do {
+
+            System.out.println("\n\nDigite a senha: ");
+            senha = new Senha(sc.nextLine());
+            senhaValida=senha.validaSenha();
+        }while(!senhaValida);
+
+        System.out.println("Força da senha: "+senha.forcaSenha());
+
     }
 }
